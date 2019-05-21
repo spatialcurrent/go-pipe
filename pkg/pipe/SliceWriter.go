a5a1d99b7a830448e692fe6a3ddd6cc4fee08faa
pjdufour <pjdufour.dev@gmail.com>
2019-05-20 23:29:53 -0500

    slice writer now accepts any object of type slice or array

--- pkg/pipe/SliceWriter.go
+++ pkg/pipe/SliceWriter.go
@@ -14,26 +14,38 @@ import (
 
 // Writer contains the WriteObject and Flush functions for writing objects.
 type SliceWriter struct {
-	values []interface{}
+	values reflect.Value
 	mutex  *sync.RWMutex
 }
 
 func NewSliceWriter() *SliceWriter {
 	return &SliceWriter{
-		values: make([]interface{}, 0),
+		values: reflect.ValueOf(make([]interface{}, 0)),
 		mutex:  &sync.RWMutex{},
 	}
 }
 
-func NewSliceWriterWithCapacity(capacity int) *SliceWriter {
+func NewSliceWriterWithValues(initialValues interface{}) *SliceWriter {
 	return &SliceWriter{
-		values: make([]interface{}, 0, capacity),
+		values: reflect.ValueOf(initialValues),
+		mutex:  &sync.RWMutex{},
+	}
+}
+
+func NewSliceWriterWithCapacity(initialCapacity int) *SliceWriter {
+	return &SliceWriter{
+		values: reflect.ValueOf(make([]interface{}, 0, initialCapacity)),
+		mutex:  &sync.RWMutex{},
 	}
 }
 
 func (sw *SliceWriter) WriteObject(object interface{}) error {
 	sw.mutex.Lock()
-	sw.values = append(sw.values, object)
+	if object == nil {
+		sw.values = reflect.Append(sw.values, reflect.Zero(sw.values.Type().Elem()))
+	} else {
+		sw.values = reflect.Append(sw.values, reflect.ValueOf(object))
+	}
 	sw.mutex.Unlock()
 	return nil
 }
@@ -42,13 +54,13 @@ func (sw *SliceWriter) Flush() error {
 	return nil
 }
 
-func (sw *SliceWriter) Values() []interface{} {
-	return sw.values
+func (sw *SliceWriter) Values() interface{} {
+	return sw.values.Interface()
 }
 
 func (sw *SliceWriter) Iterator() *SliceIterator {
 	return &SliceIterator{
-		values: reflect.ValueOf(sw.values),
+		values: sw.values,
 		cursor: 0,
 	}
 }
