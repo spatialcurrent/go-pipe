ec509977cdff55686e35a7a4f5426e9bb46e89ba
Patrick Dufour <pjdufour.dev@gmail.com>
2020-01-01 11:47:15 -0800

    SyncWriter and TransactionWriter (#6)

---
+++ pkg/pipe/SyncWriter_test.go
@@ -0,0 +1,43 @@
+// =================================================================
+//
+// Copyright (C) 2019 Spatial Current, Inc. - All Rights Reserved
+// Released as open source under the MIT License.  See LICENSE file.
+//
+// =================================================================
+
+package pipe
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSyncWriter(t *testing.T) {
+
+	values := make([]interface{}, 0)
+
+	sw := NewSyncWriter(NewFunctionWriter(func(object interface{}) error {
+		values = append(values, object)
+		return nil
+	}))
+
+	count := 1000
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < count; i++ {
+		wg.Add(1)
+		go func(i int) {
+			err := sw.WriteObject(i)
+			if err != nil {
+				panic(err)
+			}
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	assert.Lenf(t, values, count, "invalid length, expecting %d elements", count)
+}
